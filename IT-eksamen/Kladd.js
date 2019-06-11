2eb2775cd0d4dee356eb72dac2382424a4996956
mortenjrunning <mortyboy2000@hotmail.com>
2019-06-11 13:57:44 +0200

    Eksamen1

---
+++ IT-eksamen/Kladd.js
@@ -0,0 +1,23 @@
+
+//kobler til HTML
+const main = document.querySelector("main");
+const db = firebase.database();
+const messe = db.ref("Deltagere");
+
+function genererHTML(snap){
+  let messe = snap.val();
+  let key = snap.key;
+  main.innerHTML +=`
+    <div class="innhold">
+
+      <h1>${messe.navn}</h1>
+      <p>Adresse:${messe.adresse}</p>
+      <p>Type bil:${messe.type}</p>
+      <p>Ønsket plass:${messe.plass}</p>
+      <p>Tlf:${snap.key}</p>
+      <a href="messeDetaljer.html?id=${key}"> Detaljer </a>
+    </div>
+`
+}
+//on er en spørring, her på alt sammen. Sender resultatet til funksjonen genererHTML
+messe.on("child_added",genererHTML);

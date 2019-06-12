e5975dc123003b87917f1faba36e734e06576505
mortenjrunning <mortyboy2000@hotmail.com>
2019-06-12 14:47:13 +0200

    Eksamen 3

--- IT-eksamen/Kladd.js
+++ IT-eksamen/Kladd.js
@@ -21,3 +21,11 @@ function genererHTML(snap){
 }
 //on er en spørring, her på alt sammen. Sender resultatet til funksjonen genererHTML
 messe.on("child_added",genererHTML);
+
+
+<script src="Kladd.js"></script>
+Select - .on - spørring
+Insert - set -
+Deltet - remove
+Update - .on
+ (sql)    (javascript)
